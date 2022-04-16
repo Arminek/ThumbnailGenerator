db87564aa2e8e9ba23f9f0cb7aeee4bcd086d6c5
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2022-04-16 16:29:42 +0200

    [Thumbnail] Add healthy test case

---
+++ tests/Application/HealthTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\AbstractBrowser;
+use Symfony\Component\HttpFoundation\Request;
+
+final class HealthTest extends WebTestCase
+{
+    private AbstractBrowser $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+    }
+
+    /**
+     * @test
+     */
+    public function it_greets_caller(): void
+    {
+        $this->client->request(Request::METHOD_GET, '/');
+
+        $this->assertResponseIsSuccessful();
+        $content = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('message', $content);
+        $this->assertEquals('Healthy', $content['message']);
+    }
+}
