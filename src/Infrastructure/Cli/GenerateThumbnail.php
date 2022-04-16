b1734652b69e6ecfa9e0ad273f8f5814fb97afb9
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2022-04-16 21:07:59 +0200

    [Thumbnail] Create cli skeleton

--- src/Infrastructure/Cli/GenerateThumbnail.php
+++ src/Infrastructure/Cli/GenerateThumbnail.php
@@ -6,37 +6,66 @@ namespace App\Infrastructure\Cli;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 final class GenerateThumbnail extends Command
 {
-    private const DEFAULT_UPLOAD_DIR = 'upload';
+    private const DEFAULT_UPLOAD_DIR = '/upload';
 
     protected static $defaultName = 'app:generate-thumbnail';
+    private readonly Finder $finder;
+    private readonly Filesystem $filesystem;
 
     public function __construct(private readonly string $rootDir)
     {
+        $this->finder = new Finder();
+        $this->filesystem = new Filesystem();
         parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHelp('This command allows you to generate thumbnails!')
+            ->addOption(
+                'upload-dir',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                sprintf('Choose within %s as your upload directory', $this->rootDir),
+                self::DEFAULT_UPLOAD_DIR
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $helper = $this->getHelper('question');
-        $question = new ConfirmationQuestion(
-            sprintf(
-                'Do you want to change upload directory? Current (%s/%s)',
-                $this->rootDir,
-                self::DEFAULT_UPLOAD_DIR
-            ),
-            false
-        );
-        if ($helper->ask($input, $output, $question)) {
-
+        $path = $this->pathFromInput($input);
+        $output->writeln(sprintf('Current upload directory: %s', $path));
+        if (!$this->filesystem->exists($path)) {
+            $this->filesystem->mkdir($path);
         }
 
-        $output->writeln("Done!");
+        $this->finder->in($path);
+        $choices = array_map(
+            fn(\SplFileInfo $file) => $file->getFilename(),
+            iterator_to_array($this->finder->files()->getIterator())
+        );
+
+        if (count($choices) !== 0) {
+            $helper = $this->getHelper('question');
+            $question = new ChoiceQuestion('Which files?', $choices, 0);
+            $question->setMultiselect(true);
+            $helper->ask($input, $output, $question);
+        }
 
         return Command::SUCCESS;
+    }
+
+    private function pathFromInput(InputInterface $input): string
+    {
+        return sprintf('%s%s', $this->rootDir, $input->getOption('upload-dir'));
     }
 }
--- tests/Application/GenerateThumbnailTest.php
+++ tests/Application/GenerateThumbnailTest.php
@@ -7,11 +7,15 @@ namespace App\Tests\Application;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Filesystem\Filesystem;
 
 final class GenerateThumbnailTest extends KernelTestCase
 {
-    private Application $application;
+    private const TEST_UPLOAD_DIR = '/tests/upload';
+
+    private readonly Application $application;
     private readonly string $rootDir;
+    private readonly Filesystem $filesystem;
 
     protected function setUp(): void
     {
@@ -19,24 +23,85 @@ final class GenerateThumbnailTest extends KernelTestCase
 
         $kernel = self::bootKernel();
         $this->application = new Application($kernel);
+        $this->filesystem = new Filesystem();
         $this->rootDir = $kernel->getContainer()->getParameter('kernel.project_dir');
     }
 
     /**
      * @test
      */
-    public function it_generates_thumbnail(): void
+    public function it_has_default_upload_directory(): void
     {
         $command = $this->application->find('app:generate-thumbnail');
         $commandTester = new CommandTester($command);
-        $commandTester->setInputs(["no"]);
         $commandTester->execute([]);
-        $commandTester->assertCommandIsSuccessful();
+
         $output = $commandTester->getDisplay();
-        $this->assertStringContainsString(
-            sprintf('Do you want to change upload directory? Current (%s/upload)', $this->rootDir),
-            $output
-        );
-        $this->assertStringContainsString('Done!', $output);
+        $commandTester->assertCommandIsSuccessful();
+        $this->assertStringContainsString(sprintf('Current upload directory: %s/upload', $this->rootDir), $output);
+    }
+
+    /**
+     * @test
+     */
+    public function it_uses_parameter_for_upload_directory(): void
+    {
+        $command = $this->application->find('app:generate-thumbnail');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['--upload-dir' => self::TEST_UPLOAD_DIR]);
+
+        $output = $commandTester->getDisplay();
+        $commandTester->assertCommandIsSuccessful();
+        $this->assertStringContainsString(sprintf('Current upload directory: %s/tests/upload', $this->rootDir), $output);
+    }
+
+    /**
+     * @test
+     */
+    public function it_lists_files_as_options_for_thumbnail(): void
+    {
+        $images = ['a.jpg', 'b.jpg', 'c.jpg'];
+        $this->thereAreSomeImages($images);
+        $command = $this->application->find('app:generate-thumbnail');
+        $commandTester = new CommandTester($command);
+        $commandTester->setInputs(['b.jpg']);
+        $commandTester->execute(['--upload-dir' => self::TEST_UPLOAD_DIR]);
+
+        $output = $commandTester->getDisplay();
+        $commandTester->assertCommandIsSuccessful();
+        $this->assertStringContainsString('Which files?', $output);
+
+        foreach ($images as $image) {
+            $this->assertStringContainsString(sprintf('[%s/%s] %s', $this->path(), $image, $image), $output);
+        }
+    }
+
+    /**
+     * @after
+     */
+    public function purge(): void
+    {
+        if ($this->filesystem->exists($this->path())) {
+            $this->filesystem->remove($this->path());
+        }
+    }
+
+    /**
+     * @param string[] $images
+     */
+    private function thereAreSomeImages(array $images): void
+    {
+        if (!$this->filesystem->exists($this->path())) {
+            $this->filesystem->mkdir($this->path());
+        }
+
+        foreach ($images as $image) {
+            $this->filesystem->touch(sprintf('%s/%s', $this->path(), $image));
+        }
+    }
+
+    private function path(): string
+    {
+        return sprintf('%s%s', $this->rootDir, self::TEST_UPLOAD_DIR);
     }
 }
