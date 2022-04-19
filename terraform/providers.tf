1973da5b9e22857b444eda972f74e1e5cd1a1ebe
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2022-04-19 12:56:20 +0200

    [Thumbnail] Remove new york & singapore cluster

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,14 +2,6 @@ data "digitalocean_kubernetes_cluster" "fra1" {
     name = "main"
 }
 
-data "digitalocean_kubernetes_cluster" "sgp1" {
-    name = "singapore-1"
-}
-
-data "digitalocean_kubernetes_cluster" "nyc1" {
-    name = "new-york-1"
-}
-
 provider "digitalocean" {
     token = var.digital_ocean_access_token
 }
@@ -36,54 +28,6 @@ provider "helm" {
         token                  = data.digitalocean_kubernetes_cluster.fra1.kube_config[0].token
     }
     alias = "fra1"
-}
-
-provider "kubernetes" {
-    host                   = data.digitalocean_kubernetes_cluster.sgp1.endpoint
-    cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.sgp1.kube_config[0].cluster_ca_certificate)
-    token                  = data.digitalocean_kubernetes_cluster.sgp1.kube_config[0].token
-    alias                  = "sgp1"
-}
-
-provider "kubectl" {
-    host                   = data.digitalocean_kubernetes_cluster.sgp1.endpoint
-    cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.sgp1.kube_config[0].cluster_ca_certificate)
-    token                  = data.digitalocean_kubernetes_cluster.sgp1.kube_config[0].token
-    load_config_file       = false
-    alias                  = "sgp1"
-}
-
-provider "helm" {
-    kubernetes {
-        host                   = data.digitalocean_kubernetes_cluster.sgp1.endpoint
-        cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.sgp1.kube_config[0].cluster_ca_certificate)
-        token                  = data.digitalocean_kubernetes_cluster.sgp1.kube_config[0].token
-    }
-    alias = "sgp1"
-}
-
-provider "kubernetes" {
-    host                   = data.digitalocean_kubernetes_cluster.nyc1.endpoint
-    cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.nyc1.kube_config[0].cluster_ca_certificate)
-    token                  = data.digitalocean_kubernetes_cluster.nyc1.kube_config[0].token
-    alias                  = "nyc1"
-}
-
-provider "kubectl" {
-    host                   = data.digitalocean_kubernetes_cluster.nyc1.endpoint
-    cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.nyc1.kube_config[0].cluster_ca_certificate)
-    token                  = data.digitalocean_kubernetes_cluster.nyc1.kube_config[0].token
-    load_config_file       = false
-    alias                  = "nyc1"
-}
-
-provider "helm" {
-    kubernetes {
-        host                   = data.digitalocean_kubernetes_cluster.nyc1.endpoint
-        cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.nyc1.kube_config[0].cluster_ca_certificate)
-        token                  = data.digitalocean_kubernetes_cluster.nyc1.kube_config[0].token
-    }
-    alias = "nyc1"
 }
 
 provider "aws" {
